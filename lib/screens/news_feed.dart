9bef9661cef516a53dab88461a5a747b75a30d7a
harveyjavier <harveyjavier17@gmail.com>
2019-10-31 19:12:04 +0800

    Close drawer on back press

--- lib/screens/news_feed.dart
+++ lib/screens/news_feed.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:io';
 
 import 'package:bicolit/utils/uidata.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import 'package:bicolit/tools/drawer.dart';
 
 class NewsFeed extends StatefulWidget {
@@ -14,6 +14,24 @@ class NewsFeed extends StatefulWidget {
 
 class _NewsFeedState extends State<NewsFeed> {
   final storage = LocalStorage("data");
+  final RefreshController _refreshController = RefreshController();
+
+  GlobalKey<ScaffoldState> _globalKey = GlobalKey();
+  List<Widget> buildList() {
+    return List.generate(
+        15,
+        (index) => Container(
+              height: 100,
+              margin: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 15,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ));
+  }
 
   @override
   initState() {
@@ -26,23 +44,27 @@ class _NewsFeedState extends State<NewsFeed> {
   }
 
   Future<bool> _onBack() {
-    return showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Confirm Exit", style: TextStyle(color: Colors.white),),
-        content: Text("Are sure you want to exit app?", style: TextStyle(color: Colors.white),),
-        actions: <Widget>[
-          FlatButton(
-            child: Text("No", style: TextStyle(color: Colors.white),),
-            onPressed: () => Navigator.pop(context, false),
-          ),
-          FlatButton(
-            child: Text("Yes", style: TextStyle(color: Colors.white),),
-            onPressed: () => exit(0),
-          ),
-        ],
-      ),
-    );
+    if (_globalKey.currentState.isDrawerOpen) {
+      Navigator.pop(context);
+    } else {
+      return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Confirm Exit", style: TextStyle(color: Colors.white),),
+          content: Text("Are sure you want to exit app?", style: TextStyle(color: Colors.white),),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("No", style: TextStyle(color: Colors.white),),
+              onPressed: () => Navigator.pop(context, false),
+            ),
+            FlatButton(
+              child: Text("Yes", style: TextStyle(color: Colors.white),),
+              onPressed: () => exit(0),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   @override
@@ -50,12 +72,26 @@ class _NewsFeedState extends State<NewsFeed> {
     return WillPopScope(
       onWillPop: _onBack,
       child: Scaffold(
+        key: _globalKey,
         drawer: AppDrawer(current_screen: "newsFeed"),
         appBar: AppBar(
-          title: Text("NewsFeed"),
+          title: Text("News Feed"),
           centerTitle: true,
         ),
-        body: Container(),
+        //body: Container(),
+        body: SmartRefresher(
+          controller: _refreshController,
+          enablePullDown: true,
+          onRefresh: () async {
+            await Future.delayed(Duration(seconds: 1));
+            _refreshController.refreshCompleted();
+          },
+           child: CustomScrollView(
+            slivers: [
+              SliverList(delegate: SliverChildListDelegate(buildList()))
+            ],
+          ),
+        ),
       ),
     );
   }
